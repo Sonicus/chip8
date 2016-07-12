93ced78f6a9afc9ba60565aa8c387c47b5bce44e
Jere Nevalainen <jere.nevalainen@gmail.com>
2016-07-12 08:20:58 +0300

    Basis for OpCode switch case

--- src/emulator/CPU.java
+++ src/emulator/CPU.java
@@ -3,29 +3,57 @@ package emulator;
 
 import util.CpuUtil;
 
+import java.util.Stack;
+
 class CPU {
 
     private long cycleNumber;
     private short PC;
-    private short SP;
+    private Stack<Short> SP;
     private byte[] reg = new byte[16];
     private byte[] mem = new byte[4096];
     private byte[] vMem = new byte[64 * 32];
+    private boolean drawFlag;
 
     CPU(byte[] romData) {
         System.arraycopy(romData, 0, mem, 0x200, romData.length);
         PC = 0x200;
         cycleNumber = 0;
+        drawFlag = false;
     }
 
     void executeCycle() throws Exception {
         cycleNumber++;
-        short opCode = CpuUtil.shortFromBytes(mem[PC], mem[PC + 1]);
+        if(PC + 1 > mem.length-1){
+            throw new RuntimeException("PC outside memory range");
+        }
+        short opCodeShort = CpuUtil.shortFromBytes(mem[PC], mem[PC + 1]);
+        System.out.println("Current opcode is " + String.format("%04X", opCodeShort) + " (cycle " + cycleNumber + ")");
+        byte[] opCodeNibbles = CpuUtil.nibblesFromShort(opCodeShort);
+
+        switch(opCodeNibbles[0]) {
+            case 0x6:
+                break;
+            default:
+                throw new RuntimeException("Unknown OpCode " + String.format("%04X", opCodeShort));
+        }
+
         PC += 2;
-        System.out.println("Current opcode is " + String.format("%04X", opCode) + " (cycle " + cycleNumber + ")");
+    }
+
+    public long getCycleNumber() {
+        return cycleNumber;
+    }
+
+    public short getPC(){
+        return PC;
     }
 
     public byte[] getvMem() {
         return vMem;
+    }
+
+    public boolean isDrawFlag() {
+        return drawFlag;
     }
 }
