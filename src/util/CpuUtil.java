afc2e91e0096da1683c5bffea1304f57177b69b4
Jere Nevalainen <jere.nevalainen@gmail.com>
2016-07-12 08:19:41 +0300

    NibblesFromShort utility method

--- src/util/CpuUtil.java
+++ src/util/CpuUtil.java
@@ -5,4 +5,13 @@ public final class CpuUtil {
     public static short shortFromBytes(byte b1, byte b2) {
         return (short) (b1 << 8 | b2 & 0xFF);
     }
+
+    public static byte[] nibblesFromShort(short s) {
+        byte[] nibbles = new byte[4];
+        nibbles[3] = (byte) (s & 0x000F);
+        nibbles[2] = (byte) (s >> 4 & 0x000F);
+        nibbles[1] = (byte) (s >> 8 & 0x000F);
+        nibbles[0] = (byte) (s >> 12 & 0x000F);
+        return nibbles;
+    }
 }
